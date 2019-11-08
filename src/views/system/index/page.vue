b29835ac046776a8596300cec0ed1362f06bf5be
liux <77398662@qq.com>
2019-11-08 13:05:13 +0800

    s

---
+++ src/views/system/index/page.vue
@@ -0,0 +1,42 @@
+<template>
+  <d2-container class="page">
+   首页
+  </d2-container>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      filename: __filename
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  .logo {
+    width: 120px;
+  }
+  .btn-group {
+    color: $color-text-placehoder;
+    font-size: 12px;
+    margin-top: 0px;
+    margin-bottom: 20px;
+    .btn-group__btn {
+      color: $color-text-sub;
+      &:hover {
+        color: $color-text-main;
+      }
+      &.btn-group__btn--link {
+        color: $color-primary;
+      }
+    }
+  }
+}
+</style>
