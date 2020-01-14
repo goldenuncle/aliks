b8937e934b844239824cfa9c3b188d5c664fcaf7
goldenuncle <golenuncle@163.com>
2020-01-14 21:50:03 +0800

    2020-1-14

--- src/views/blog/components/navmemu/index.vue
+++ src/views/blog/components/navmemu/index.vue
@@ -1,52 +1,53 @@
 <template>
-<div class="header">
-    <div calss="container">
-        <el-image class="logoimg"
-      style="width: 146px; height: 38px"
-      :src="/.s"
-      :fit="fit"></el-image>
-        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" class="memu">
-            <el-menu-item index="1">
-            </el-menu-item>
-            <el-menu-item index="2">处理中心</el-menu-item>
-            
-            <el-menu-item index="3" disabled>消息中心</el-menu-item>
-            <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-        </el-menu>
+  <div class="header">
+    <div class="logo">
+      <el-image
+      style="width: 146px; height: 42px"
+      :src="url"
+      fit="fit"></el-image>
     </div>
-
-</div>
-
+    <div class="memu">
+       <el-menu router :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" active-text-color="#FF69B4">
+        <el-menu-item index="1" route="/">首页</el-menu-item>
+        <el-menu-item index="2" route="/article">文章</el-menu-item>
+        <el-menu-item index="3" route="/about">关于我
+        </el-menu-item>
+      </el-menu>
+    </div>
+   
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-      return {
-        activeIndex: '1',
-        logoUrl:'@/public/image/blog.img/logo.png'
-      };
-    },
-    methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
+  data() {
+    return {
+      activeIndex: "1",
+      url: "https://goldenuncle.cn/image/logo.png"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
+};
 </script>
 
-<style lang="scss" scoped>
-.header{
-    .container{
-        margin:0 auto; 
-        width:400px; 
-        height:100px; 
-        border:1px solid #F00;
-        display: flex;
-        flex-direction: row;
-        .logo{
-            flex: auto;
-        }
-    }
+<style  lang="scss" scoped>
+.header {
+  margin:0 auto;
+  display: flex;
+  justify-content:space-around;
+  align-items:  center ;
+  .logo{
+    width: 146px;
+    text-align: center;
+    margin: 0;
+    font-weight: 400
+  }
+  .memu{
+
+  }
 }
 </style>
--- src/views/blog/index/index.vue
+++ src/views/blog/index/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="blog"> 
       <navMemu/>
+      <el-row :gutter="10">
+        
+        <el-col :offset="3" :xs="8" :sm="9" :md="10" :lg=11 :xl="12"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :xs="1" :sm="2" :md="4" :lg="6" :xl="8"><div class="grid-content bg-purple"></div></el-col>
+
+      </el-row>
       <router-view/>
   </div>
 </template>
@@ -14,6 +20,25 @@ components: {
 }
 </script>
 
-<style>
-
+<style  lang="scss" scoped>
+.el-row{
+  margin: 0!important;
+  padding: 0!important;
+}
+.el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
 </style>
